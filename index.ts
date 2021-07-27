4157ef7f87f5d9599d05eeb5a1e0d631f893f112
konst <konstantin.mikheev@icord.cz>
2021-07-27 14:43:41 +0200

    url-parser model

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from './common';
 export * from './yahoo';
 export * from './twitter';
+export * from './url-parser';
---
+++ url-parser/index.ts
@@ -0,0 +1,3 @@
+export interface UrlParseResponse {
+  imgUrl: string | null;
+}
